696cebc9b747e18aaf17a128388ac1a17fdb894d
teepunkt-esspunkt <tareks-mail@proton.me>
2026-03-11 10:17:57 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 <x-layout title="Startseite">
 <h1>Task-Manager</h1>
-<p>Der Task-Manager dient dazu Aufgaben zu zeigen, anzulegen, zu aktualisieren und zu löschen.</p>
+<p>Der Task-Manager dient dazu Aufgaben zu zeigen, anzulegen, zu aktualisieren und zu löschen!</p>
 </x-layout>
